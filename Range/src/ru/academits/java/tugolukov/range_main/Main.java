6c1b02b854465d4abe3af5430936b0c42cce9226
Valery0904 <valery.tugolukov@gmail.com>
2025-01-25 12:29:44 +0700

    Задача Range часть 1

--- Range/src/ru/academits/java/tugolukov/range_main/Main.java
+++ Range/src/ru/academits/java/tugolukov/range_main/Main.java
@@ -1,7 +1,30 @@
 package ru.academits.java.tugolukov.range_main;
 
+import ru.academits.java.tugolukov.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите начало числового диапазона: ");
+        double from = scanner.nextDouble();
+
+        System.out.print("Введите конец числового диапазона: ");
+        double to = scanner.nextDouble();
+
+        System.out.print("Введите число которое хотите проверить: ");
+        double number = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+
+        System.out.printf("Длина числового диапазона от %.2f до %.2f = %.2f%n", range.getFrom(), range.getTo(), range.getLength());
+
+        if (range.isInside(number)) {
+            System.out.printf("Число %.2f принадлежит диапазону", number);
+        } else {
+            System.out.printf("Число %.2f не принадлежит диапазону", number);
+        }
     }
 }
