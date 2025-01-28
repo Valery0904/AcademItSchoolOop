114aefa0cd79c2d8ed989474761ce8f2f9f8f4b0
Valery Tugolukov <valery.tugolukov@gmail.com>
2025-01-28 21:39:22 +0700

    Создание класса Rectangle

---
+++ Shapes/src/ru/academits/java/tugolukov/shapes/Rectangle.java
@@ -0,0 +1,31 @@
+package ru.academits.java.tugolukov.shapes;
+
+public class Rectangle implements Shapes {
+    public double height;
+    public double width;
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double getArea() {
+        return height * width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+}
