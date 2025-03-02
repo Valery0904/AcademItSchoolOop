f9fad935a9e47fc72c85126b1982120cd1d93dca
Valery Tugolukov <valery.tugolukov@gmail.com>
2025-03-02 10:34:27 +0700

    Vector

--- Vector/src/ru/academits/java/tugolukov/vector/Vector.java
+++ Vector/src/ru/academits/java/tugolukov/vector/Vector.java
@@ -1,6 +1,5 @@
 package ru.academits.java.tugolukov.vector;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Vector {
@@ -12,13 +11,13 @@ public class Vector {
             System.out.println("Размерность вектора должна быть больше 0.");
         }
 
-        this.components = new double[n];
+        components = new double[n];
     }
 
     // Vector(Vector) – конструктор копирования
-    public Vector (Vector vector) {
-        this.components = new double[vector.components.length];
-        System.arraycopy(vector.components, 0, this.components, 0, vector.components.length);
+    public Vector(Vector vector) {
+        components = new double[vector.components.length];
+        System.arraycopy(vector.components, 0, components, 0, vector.components.length);
     }
 
     // Vector(double[]) – заполнение вектора значениями из массива
@@ -27,8 +26,8 @@ public class Vector {
             System.out.println("Размерность вектора должна быть больше 0.");
         }
 
-        this.components = new double[array.length];
-        System.arraycopy(array, 0, this.components, 0, array.length);
+        components = new double[array.length];
+        System.arraycopy(array, 0, components, 0, array.length);
     }
 
     // Vector(n, double[]) – заполнение вектора значениями из массива.
@@ -38,23 +37,143 @@ public class Vector {
             System.out.println("Размерность вектора должна быть больше 0.");
         }
 
-        this.components = new double[n];
+        components = new double[n];
         int componentsToCopy = Math.min(array.length, n);
 
-        System.arraycopy(array, 0, this.components, 0, componentsToCopy);
+        System.arraycopy(array, 0, components, 0, componentsToCopy);
     }
 
     // Метод getSize() для получения размерности вектора
-    public int getSize(){
+    public int getSize() {
         return components.length;
     }
 
     // Реализовать метод toString(), чтобы выдавал информацию о векторе
-    // в  формате { значения компонент через запятую } Например, {1, 2, 3}
+    // в формате {значения компонент через запятую} Например, {1, 2, 3}
     @Override
     public String toString() {
         return "Vector{" + Arrays.toString(components) + '}';
     }
 
+    // Прибавление к вектору другого вектора
+    public void addVector(Vector vector) {
+        for (int i = 0; i < Math.min(getSize(), vector.getSize()); i++) {
+            components[i] += vector.components[i];
+        }
+    }
 
+    // Вычитание вектора
+    public void subtractVector(Vector vector) {
+        for (int i = 0; i < components.length; i++) {
+            components[i] -= vector.components[i];
+        }
+    }
+
+    // Умножение вектора на скаляр
+    public void multiplyByScalar(double scalar) {
+        for (int i = 0; i < components.length; i++) {
+            components[i] *= scalar;
+        }
+    }
+
+    // Разворот вектора
+    public void reverseVector() {
+        multiplyByScalar(-1);
+    }
+
+    // Получение длины вектора
+    public double getLength() {
+        double sum = 0;
+
+        for (double component : components) {
+            sum += component * component;
+        }
+
+        return Math.sqrt(sum);
+    }
+
+    // Получение компоненты по индексу
+    public double getComponent(int index) {
+        checkIndex(index);
+
+        return components[index];
+    }
+
+    // Установка компоненты по индексу
+    public void setComponent(int index, double value) {
+        checkIndex(index);
+        components[index] = value;
+    }
+
+    // Проверка индекса
+    private void checkIndex(int index) {
+        if (index < 0 || index > components.length) {
+            throw new IndexOutOfBoundsException("Индекс выходит за пределы вектора");
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) obj;
+
+        return Arrays.equals(components, vector.components);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(components);
+    }
+
+    // Статическое сложение векторов
+    public static Vector addVector(Vector v1, Vector v2) {
+        int maxSize = Math.max(v1.getSize(), v2.getSize());
+        double[] resultComponents = new double[maxSize];
+
+        for (int i = 0; i < maxSize; i++) {
+            double component1 = i < v1.getSize() ? v1.components[i] : 0;
+            double component2 = i < v2.getSize() ? v2.components[i] : 0;
+
+            resultComponents[i] = component1 + component2;
+        }
+
+        return new Vector(resultComponents);
+    }
+
+    // Статическое вычитание векторов
+    public static Vector subtractVector(Vector v1, Vector v2) {
+        int maxSize = Math.max(v1.getSize(), v2.getSize());
+        double[] resultComponents = new double[maxSize];
+
+        for (int i = 0; i < maxSize; i++) {
+            double component1 = i < v1.getSize() ? v1.components[i] : 0;
+            double component2 = i < v2.getSize() ? v2.components[i] : 0;
+
+            resultComponents[i] = component1 - component2;
+        }
+
+        return new Vector(resultComponents);
+    }
+
+    // Статическое скалярное произведение
+    public static double dotProduct(Vector v1, Vector v2) {
+        int maxSize = Math.max(v1.getSize(), v2.getSize());
+        double result = 0;
+
+        for (int i = 0; i < maxSize; i++) {
+            double component1 = i < v1.getSize() ? v1.components[i] : 0;
+            double component2 = i < v2.getSize() ? v2.components[i] : 0;
+
+            result = component1 * component2;
+        }
+
+        return result;
+    }
 }
--- Vector/src/ru/academits/java/tugolukov/vector_main/Main.java
+++ Vector/src/ru/academits/java/tugolukov/vector_main/Main.java
@@ -4,9 +4,39 @@ import ru.academits.java.tugolukov.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(new double[]{2.2, 3.1, 4.0, 5.2});
+        Vector v1 = new Vector(3);
+        Vector v2 = new Vector(new double[]{1.0, 2.0, 3.0});
+        Vector v3 = new Vector(4, new double[]{1.0, 2.0});
 
-        System.out.println(vector);
-        System.out.println(vector.getSize());
+        System.out.println("v1: " + v1);
+        System.out.println("v2: " + v2);
+        System.out.println("v3: " + v3);
+
+        v1.addVector(v2);
+        System.out.println("v1 после прибавления v2: " + v1);
+
+        v1.subtractVector(v3);
+        System.out.println("v1 после вычитания v3: " + v1);
+
+        v1.multiplyByScalar(2);
+        System.out.println("v1 после умножения на скаляр 2: " + v1);
+
+        v1.reverseVector();
+        System.out.println("v1 после разворота: " + v1);
+
+        System.out.println("Длина v1: " + v1.getLength());
+
+        System.out.println("Компонента v2[1]: " + v2.getComponent(1));
+        v2.setComponent(1, 5.0);
+        System.out.println("v2 после изменения компоненты: " + v2);
+
+        Vector v4 = Vector.addVector(v2, v3);
+        System.out.println("Сложение v2 и v3: " + v4);
+
+        Vector v5 = Vector.subtractVector(v2, v3);
+        System.out.println("Вычитание v3 из v2: " + v5);
+
+        double dotProduct = Vector.dotProduct(v2, v3);
+        System.out.println("Скалярное произведение v2 и v3: " + dotProduct);
     }
 }
